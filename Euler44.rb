ac5eead75714f2b8e38f64cd08c87ac0a98cd551
christian.guenter <chguenter@web.de>
2012-12-29 07:49:36 +0100

    Solution

--- Euler44.rb
+++ Euler44.rb
@@ -1,3 +1,32 @@
+class Pentagonal
+  def self.get_pentagonal(limit)
+    1.upto(limit) do |x|
+      yield x * (3 * x - 1) / 2 if block_given?
+    end
+  end
+end
+
+class Integer
+  def pent?
+    ((1+Math.sqrt(1+24*self.abs))/6)%1 == 0
+  end
+end
+
 class Euler44
+
   
+  def find_result
+    pen_num = Array.new
+    Pentagonal.get_pentagonal(3000){|x| pen_num << x}
+    
+    d = nil
+    pen_num.each do |x|
+      pen_num.each do |y|        
+        if (x - y).pent? && (x + y).pent?
+          d = (x - y).abs if d == nil || d > (x - y).abs
+        end
+      end
+    end
+    return d
+  end
 end
--- TestEuler44.rb
+++ TestEuler44.rb
@@ -3,6 +3,10 @@ require 'test/unit'
 
 class TestEuler44 < Test::Unit::TestCase
   
+  def test_find_result
+    assert_equal(5482660,@euler44.find_result)
+  end
+  
   def setup 
     @euler44 = Euler44.new
   end
