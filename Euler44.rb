62e107aff178173b8f7ed5bb4fd50d1a4db77ff0
christian.guenter <chguenter@web.de>
2012-12-29 06:11:50 +0100

    inital classes

---
+++ Euler44.rb
@@ -0,0 +1,3 @@
+class Euler44
+  
+end
---
+++ TestEuler44.rb
@@ -0,0 +1,10 @@
+require_relative 'Euler44'
+require 'test/unit'
+
+class TestEuler44 < Test::Unit::TestCase
+  
+  def setup 
+    @euler44 = Euler44.new
+  end
+  
+end
